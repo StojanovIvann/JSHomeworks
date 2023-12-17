496660f3aafc41f738cfe1d359198b73eca7eef0
StojanovIvann <stojanov0011@gmail.com>
2023-12-17 16:01:05 +0100

    domasna

--- Class06/script.js
+++ Class06/script.js
@@ -5,15 +5,19 @@ btn.addEventListener('click', function createTable(){
     let rows = parseInt(document.getElementById('rows').value);
     let columns = parseInt(document.getElementById('columns').value);
     let table = '<table class="table" border="1px">';
+    let text = " ";
 
     if ((rows !== NaN) && (columns !==NaN)){
     
     for (let i = 0; i < rows; i++) {
-      table += '<tr>  </tr>';
+      table += `<tr>   </tr>`;
+  
 
-      for (let a = 0; a < columns; a++) {
-        table += '<td style = "padding:30px" > </td>';
+      for (let j = 0; j < columns; j++) {
+        table += `<td style = "padding:30px" > Row ${i + 1} Column ${j + 1} </td>`;
+        
       }
+      
     } 
     table += '</table>';
     let main =  document.getElementById('tableContainer');
